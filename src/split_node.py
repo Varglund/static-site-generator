192d8b1c490084032bbe9efda760661115b516ee
Varglund <sean.varglund@gmail.com>
2025-01-24 23:42:42 +0200

    maybe a bit short on content. Check against solution files.

--- src/split_node.py
+++ src/split_node.py
@@ -13,6 +13,9 @@ def split_nodes_delimiter(old_nodes: List[TextNode], delimiter: Delimiter, text_
         raise ValueError("no nodes provided")
     new_nodes = []
     for node in old_nodes:
+        if node.text_type != TextType.NORMAL_TEXT:
+            new_nodes.append(node)
+            continue
         fragments = node.text.split(sep=delimiter.value, maxsplit=3)
         no_frags = len(fragments)
         if no_frags==3:
@@ -26,3 +29,9 @@ def split_nodes_delimiter(old_nodes: List[TextNode], delimiter: Delimiter, text_
         else:
             raise ValueError(f"Text '{node.text}' only contained one copy of {delimiter.value=}")
     return new_nodes
+
+def split_all_nodes(old_nodes: List[TextNode])->List[TextNode]:
+    bolds = split_nodes_delimiter(old_nodes, Delimiter.BOLD, TextType.BOLD_TEXT)
+    italics = split_nodes_delimiter(bolds, Delimiter.ITALIC, TextType.ITALIC_TEXT)
+    codes = split_nodes_delimiter(italics, Delimiter.CODE, TextType.CODE_TEXT)
+    return codes
