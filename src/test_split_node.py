d834060907971e7bfbceb78dd4ad998465e718ed
Varglund <sean.varglund@gmail.com>
2025-01-24 23:22:43 +0200

    Add test for multiple input nodes

--- src/test_split_node.py
+++ src/test_split_node.py
@@ -34,6 +34,22 @@ class TestSplitNode(unittest.TestCase):
         ]
         self.assertEqual(new_nodes, expected)
     
+    def test_split_multiple_bold(self):
+        node = TextNode("This is text with an **bold block** word", TextType.NORMAL_TEXT)
+        new_nodes = split_nodes_delimiter([node, node, node], Delimiter.BOLD, TextType.BOLD_TEXT)
+        expected = [
+            TextNode("This is text with an ", TextType.NORMAL_TEXT),
+            TextNode("bold block", TextType.BOLD_TEXT),
+            TextNode(" word", TextType.NORMAL_TEXT),
+            TextNode("This is text with an ", TextType.NORMAL_TEXT),
+            TextNode("bold block", TextType.BOLD_TEXT),
+            TextNode(" word", TextType.NORMAL_TEXT),
+            TextNode("This is text with an ", TextType.NORMAL_TEXT),
+            TextNode("bold block", TextType.BOLD_TEXT),
+            TextNode(" word", TextType.NORMAL_TEXT),
+        ]
+        self.assertEqual(new_nodes, expected)
+    
     def test_delimiter_not_found_return_node(self):
         node = TextNode("This is text with a `code block` word", TextType.NORMAL_TEXT)
         new_nodes = split_nodes_delimiter([node], Delimiter.ITALIC, TextType.ITALIC_TEXT)
