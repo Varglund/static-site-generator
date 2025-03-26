3f46e94a0e83dc17da984994b8b09ccd045cec4e
Sean Varglund <sean.varglund@gmail.com>
2025-03-26 18:04:15 +0200

    After successfully making the recursive function, opt for syntax from BootDev

--- src/file_handling.py
+++ src/file_handling.py
@@ -1,23 +1,15 @@
 import os
 import shutil
-import logging
 
-logging.basicConfig(filename=os.path.join("logging","copy_logs.log"), level=logging.DEBUG)
+def copy_files_recursive(source_dir_path, dest_dir_path):
+    if not os.path.exists(dest_dir_path):
+        os.mkdir(dest_dir_path)
 
-def initialize_dir(dir_path:os.PathLike)->None:
-    if os.path.exists(dir_path):
-        shutil.rmtree(dir_path)
-    os.mkdir(dir_path)
-
-def recursive_copy(from_path:os.PathLike, to_path:os.PathLike)->None:
-    if os.path.isfile(from_path):
-        shutil.copy(from_path, to_path)
-        return None
-    elif not os.path.exists(from_path):
-        raise ValueError(f"The directory {from_path=} does not exist.")
-    logging.debug(f"Initializing {to_path=}")
-    initialize_dir(to_path)
-    for item in (contents:=os.listdir(from_path)):
-        logging.debug(f"Handling {item} from {contents=}")
-        logging.debug(f"Calling recursive copy on {os.path.join(from_path, item)} to {os.path.join(to_path, item)}.")
-        recursive_copy(os.path.join(from_path, item), os.path.join(to_path, item))
+    for filename in os.listdir(source_dir_path):
+        from_path = os.path.join(source_dir_path, filename)
+        dest_path = os.path.join(dest_dir_path, filename)
+        print(f" * {from_path} -> {dest_path}")
+        if os.path.isfile(from_path):
+            shutil.copy(from_path, dest_path)
+        else:
+            copy_files_recursive(from_path, dest_path)
